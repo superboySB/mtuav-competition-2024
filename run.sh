902d8b2c91976143f4039bafd8bcb3e9a87626a5
Zipeng Dai <604896160@qq.com>
2024-09-05 16:03:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "正在执行比赛程序"
 # 在这里添加您的比赛程序代码运行指令
 
 # Source your workspace's setup.bash file
-source /home/mtuav-competition-2024/devel/setup.bash  # 根据你的工作空间路径修改
+source ~/mtuav-competition-2024/devel/setup.bash  # 根据你的工作空间路径修改
 
 # Run the ROS node
 rosrun race_demo demo.py
